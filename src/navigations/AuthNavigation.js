972ec26ed0caea66a20dbb13e282870d2d014bcb
Jay <jaypithava@xhtmljunkies.com>
2023-01-05 12:51:15 +0530

    Navigation and design added

---
+++ src/navigations/AuthNavigation.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Login, ForgotPassword, Register } from "../screens";
+import { COLORS, ROUTES } from "../constants";
+
+const Stack = createNativeStackNavigator();
+
+const AuthNavigation = () => {
+  console.log(Stack);
+  return (
+    <Stack.Navigator screenOptions={{}} initialRouteName={ROUTES.LOGIN}>
+      <Stack.Screen
+        name={ROUTES.FORGOT_PASSWORD}
+        component={ForgotPassword}
+        options={({route}) => ({
+          headerTintColor: COLORS.white,
+          // headerBackTitle: 'Back',
+          headerBackTitleVisible: false,
+          headerStyle: {
+            backgroundColor: COLORS.primary,
+          },
+          title: route.params.userId,
+        })}
+      />
+      <Stack.Screen
+        name={ROUTES.LOGIN}
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name={ROUTES.REGISTER} component={Register} />
+      <Stack.Screen
+        name={ROUTES.HOME}
+        component={Register}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
+  );
+
+};
+
+export default AuthNavigation;
